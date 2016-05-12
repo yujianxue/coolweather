f088bf1c1e2ac54d8408a4fb8afb0bdd3397d807
qiuxuejing <xuejingqiu@sohu-inc.com>
2016-05-12 11:31:54 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2016/5/11.
+ */
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+
+}
